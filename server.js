0b99217e1cff0581c0a5b57d7b80697dafc80e0d
kgideon084-maker <kgideon084@gmail.com>
2026-07-11 11:41:37 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('.'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
